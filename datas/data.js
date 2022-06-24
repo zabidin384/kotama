7fce80fa3afb535fed38deede2921e58e9d58a66
zabidin384 <90741858+zabidin384@users.noreply.github.com>
2022-06-24 22:35:44 +0700

    Add files via upload

---
+++ datas/data.js
@@ -0,0 +1,161 @@
+export const dashboards = [
+	{ name: "Printer", category: "Hardware", amount: 123, conditionB: 121, conditionRR: 0, conditionRB: 0, top: 123 },
+	{ name: "Aplikasi", category: "Software", amount: 123, conditionB: 121, conditionRR: 0, conditionRB: 0, top: 123 },
+	{ name: "Komputer", category: "Hardware", amount: 123, conditionB: 121, conditionRR: 0, conditionRB: 0, top: 123 },
+	{ name: "Server", category: "Hardware", amount: 123, conditionB: 121, conditionRR: 0, conditionRB: 0, top: 123 },
+	{ name: "Server", category: "Hardware", amount: 123, conditionB: 121, conditionRR: 0, conditionRB: 0, top: 123 },
+	{ name: "Server", category: "Hardware", amount: 123, conditionB: 121, conditionRR: 0, conditionRB: 0, top: 123 },
+];
+
+export const kodams = [
+	{ img: "/images/dashboard/kodam1.png", name: "KODAM I/BB" },
+	{ img: "/images/dashboard/kodam2.png", name: "KODAM II/BB" },
+	{ img: "/images/dashboard/kodam3.png", name: "KODAM III/BB" },
+	{ img: "/images/dashboard/kodam4.png", name: "KODAM IV/BB" },
+	{ img: "/images/dashboard/kodam4.png", name: "KODAM V/BB" },
+	{ img: "/images/dashboard/kodam4.png", name: "KODAM VI/BB" },
+	{ img: "/images/dashboard/kodam4.png", name: "KODAM VII/BB" },
+	{ img: "/images/dashboard/kodam4.png", name: "KODAM VIII/BB" },
+	{ img: "/images/dashboard/kodam4.png", name: "KODAM IX/BB" },
+	{ img: "/images/dashboard/kodam4.png", name: "KODAM X/BB" },
+];
+
+export const kotamas = [
+	{ img: "/images/kotama/kodam1.png", name: "KODAM I/BB" },
+	{ img: "/images/kotama/kodam2.png", name: "KODAM II/BB" },
+	{ img: "/images/kotama/kodam3.png", name: "KODAM III/BB" },
+	{ img: "/images/kotama/kodam4.png", name: "KODAM IV/BB" },
+	{ img: "/images/kotama/kodam1.png", name: "KODAM I/BB" },
+	{ img: "/images/kotama/kodam2.png", name: "KODAM II/BB" },
+	{ img: "/images/kotama/kodam3.png", name: "KODAM III/BB" },
+	{ img: "/images/kotama/kodam4.png", name: "KODAM IV/BB" },
+	{ img: "/images/kotama/kodam1.png", name: "KODAM I/BB" },
+	{ img: "/images/kotama/kodam2.png", name: "KODAM II/BB" },
+	{ img: "/images/kotama/kodam3.png", name: "KODAM III/BB" },
+	{ img: "/images/kotama/kodam4.png", name: "KODAM IV/BB" },
+];
+
+export const kotamaDetails = [
+	{ satminkal: "INFOLAHTADAM IV/DIP", software: 1, hardware: 12, taPengadaan: 123 },
+	{ satminkal: "SLOGDAM IV/DIP", software: 1, hardware: 12, taPengadaan: 123 },
+	{ satminkal: "RINDAM IV/DIP", software: 1, hardware: 12, taPengadaan: 123 },
+	{ satminkal: "ZIDAM IV/DIP", software: 1, hardware: 12, taPengadaan: 123 },
+];
+
+export const kodamDetails = [
+	{ name: "Printer", category: "Hardware", amount: 123, conditionB: 121, conditionRR: 0, conditionRB: 0, top: 123 },
+	{ name: "Aplikasi", category: "Software", amount: 123, conditionB: 121, conditionRR: 0, conditionRB: 0, top: 123 },
+	{ name: "Komputer", category: "Hardware", amount: 123, conditionB: 121, conditionRR: 0, conditionRB: 0, top: 123 },
+	{ name: "Server", category: "Hardware", amount: 123, conditionB: 121, conditionRR: 0, conditionRB: 0, top: 123 },
+];
+
+export const products = [
+	{ name: "Printer", category: "Hardware", price: 900000, stock: 123, merk: "Tes", sn: "Tes", x: "Tes", taPengadaan: "Tes" },
+	{ name: "Aplikasi", category: "Software", price: 120000, stock: 123, merk: "Tes", sn: "Tes", x: "Tes", taPengadaan: "Tes" },
+	{ name: "Komputer", category: "Hardware", price: 2000000, stock: 123, merk: "Tes", sn: "Tes", x: "Tes", taPengadaan: "Tes" },
+	{ name: "Server", category: "Hardware", price: 500000, stock: 123, merk: "Tes", sn: "Tes", x: "Tes", taPengadaan: "Tes" },
+];
+
+export const productDetails = [
+	{ kotama: "Kodam V", satminkal: "INFOLAHTADAMV/BRW", conditionB: 100, conditionRR: 100, conditionRB: 100, top: 100 },
+	{ kotama: "Kodam V", satminkal: "SLOGDAM V/BRW", conditionB: 100, conditionRR: 100, conditionRB: 100, top: 100 },
+];
+
+export const tables = [
+	{
+		kotama: "Tes",
+		satminkal: "Tes",
+		category: "Tes",
+		product: "Tes",
+		price: "Tes",
+		stock: "Tes",
+		merk: "Tes",
+		sn: "Tes",
+		condition: "Tes",
+		taPengadaan: "Tes",
+	},
+	{
+		kotama: "Tes",
+		satminkal: "Tes",
+		category: "Tes",
+		product: "Tes",
+		price: "Tes",
+		stock: "Tes",
+		merk: "Tes",
+		sn: "Tes",
+		condition: "Tes",
+		taPengadaan: "Tes",
+	},
+	{
+		kotama: "Tes",
+		satminkal: "Tes",
+		category: "Tes",
+		product: "Tes",
+		price: "Tes",
+		stock: "Tes",
+		merk: "Tes",
+		sn: "Tes",
+		condition: "Tes",
+		taPengadaan: "Tes",
+	},
+	{
+		kotama: "Tes",
+		satminkal: "Tes",
+		category: "Tes",
+		product: "Tes",
+		price: "Tes",
+		stock: "Tes",
+		merk: "Tes",
+		sn: "Tes",
+		condition: "Tes",
+		taPengadaan: "Tes",
+	},
+	{
+		kotama: "Tes",
+		satminkal: "Tes",
+		category: "Tes",
+		product: "Tes",
+		price: "Tes",
+		stock: "Tes",
+		merk: "Tes",
+		sn: "Tes",
+		condition: "Tes",
+		taPengadaan: "Tes",
+	},
+	{
+		kotama: "Tes",
+		satminkal: "Tes",
+		category: "Tes",
+		product: "Tes",
+		price: "Tes",
+		stock: "Tes",
+		merk: "Tes",
+		sn: "Tes",
+		condition: "Tes",
+		taPengadaan: "Tes",
+	},
+	{
+		kotama: "Tes",
+		satminkal: "Tes",
+		category: "Tes",
+		product: "Tes",
+		price: "Tes",
+		stock: "Tes",
+		merk: "Tes",
+		sn: "Tes",
+		condition: "Tes",
+		taPengadaan: "Tes",
+	},
+	{
+		kotama: "Tes",
+		satminkal: "Tes",
+		category: "Tes",
+		product: "Tes",
+		price: "Tes",
+		stock: "Tes",
+		merk: "Tes",
+		sn: "Tes",
+		condition: "Tes",
+		taPengadaan: "Tes",
+	},
+];
